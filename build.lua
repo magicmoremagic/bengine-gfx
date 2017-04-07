bbef55102cfaa0f2b685def90a58811aacab1edd
Ben Crist <ben@magicmoremagic.com>
2017-04-07 00:12:36 -0500

    extracting in-memory texture/image processing to be::gfx::tex

--- build.lua
+++ build.lua
@@ -1,15 +1,24 @@
 module 'gfx' {
+   lib '-tex' {
+      include 'include/tex',
+      src {
+         'src/tex/*.cpp',
+         pch_src 'src/tex/pch.cpp'
+      },
+      src {
+         'src/tex/*.c',
+      },
+      define 'BE_GFX_TEX_IMPL',
+      link_project 'core'
+   },
    lib {
       src {
          'src/*.cpp',
          pch_src 'src/pch.cpp'
       },
-      src {
-         'src/*.c',
-      },
+      
       define 'BE_GFX_IMPL',
-      link_project 'core',
-      link_project 'util'
+      link_project { 'core', 'util', 'gfx-tex' }
    },
    app '-test' {
       icon 'icon/bengine-test-perf.ico',
---
+++ include/tex/pch.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#ifndef BE_PCH_HPP_
+#define BE_PCH_HPP_
+
+// #ifndef BE_PCH_NO_DEPS
+// #  include <gsl/gsl>
+// #  ifdef BE_PCH_GLM
+// #     include <glm/glm.hpp>
+// #  endif
+// #  ifdef BE_PCH_SQLITE
+// #     include <sqlite3.h>
+// #  endif
+// #endif
+// #ifndef BE_PCH_NO_BOOST
+// #  ifdef BE_PCH_BOOST_ASIO
+// #     include <boost/asio/io_service.hpp>
+// #  endif
+// #  ifdef BE_PCH_BOOST_TYPE_ERASURE
+// #     include <boost/type_erasure/any.hpp>
+// #     include <boost/type_erasure/operators.hpp>
+// #     include <boost/mpl/vector.hpp>
+// #  endif
+// #  ifdef BE_PCH_BOOST_CONTAINER
+// #     include <boost/container/deque.hpp>
+// #  endif
+// #endif
+// #ifndef BE_PCH_NO_CORE
+// #  include <be/core/be.hpp>
+// #  include <be/core/alg.hpp>
+// #  include <be/core/services.hpp>
+// #  include <be/core/buf.hpp>
+// #  include <be/core/id.hpp>
+// #  ifndef BE_PCH_NO_IO
+// #     include <be/core/console.hpp>
+// #     include <be/core/logging.hpp>
+// #  endif
+// #endif
+// #ifndef BE_PCH_NO_STD
+// #  include <cstddef>
+// #  include <cstdint>
+// #  include <utility>
+// #  include <type_traits>
+// #  include <memory>
+// #  include <algorithm>
+// #  include <numeric>
+// #  include <vector>
+// #  include <array>
+// #  include <unordered_map>
+// #  include <unordered_set>
+// #  include <functional>
+// #  include <limits>
+// #  ifndef BE_PCH_NO_IO
+// #     include <iostream>
+// #     include <sstream>
+// #     include <iomanip>
+// #  endif
+// #endif
+#endif

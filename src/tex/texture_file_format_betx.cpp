76b2e6d5a12c76756ac5bf62e9a6f3eef2d5e9ae
Ben Crist <ben@magicmoremagic.com>
2017-05-16 01:30:23 -0500

    betx signature

--- src/tex/texture_file_format_betx.cpp
+++ src/tex/texture_file_format_betx.cpp
@@ -6,8 +6,11 @@ namespace be::gfx::tex::detail {
 
 ///////////////////////////////////////////////////////////////////////////////
 bool is_betx(const Buf<const UC>& buf) {
-   // TODO
-   constexpr UC signature[] = { 0x57, 0xcf, 'b', 'e', 'T', 'x', '\r', '\n', 0x1a, '\n' };
+   // #57c0 - magicmoremagic prefix
+   // 'beTx' - canonical extension
+   // #0D0A1A0A - see PNG signature rationale
+   // #4FE939FD - idgen -f canonical -s 32 "beTx - bengine texture file format"
+   constexpr UC signature[] = { 0x57, 0xc0, 'b', 'e', 'T', 'x', '\r', '\n', 0x1a, '\n', 0x4F, 0xE9, 0x39, 0xFD };
    return util::file_signature_matches(buf, signature);
 }
 

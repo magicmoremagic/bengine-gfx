2c127ec07438e73d5657b9df16525167c5f15b68
Ben Crist <ben@magicmoremagic.com>
2017-09-24 00:31:25 -0500

    bgl enum_name()

--- include/version.hpp
+++ include/version.hpp
@@ -6,7 +6,7 @@
 
 #define BE_GFX_VERSION_MAJOR 0
 #define BE_GFX_VERSION_MINOR 1
-#define BE_GFX_VERSION_REV 26
+#define BE_GFX_VERSION_REV 27
 
 /*!! include('common/version', 'be::gfx') !! 6 */
 /* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */

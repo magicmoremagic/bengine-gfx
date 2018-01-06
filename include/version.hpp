1db0d845a995c0e464b5a4d79dcd10e67c53be4e
Ben Crist <ben@magicmoremagic.com>
2018-01-06 01:02:51 -0600

    windows sdk version bump

--- include/version.hpp
+++ include/version.hpp
@@ -6,7 +6,7 @@
 
 #define BE_GFX_VERSION_MAJOR 0
 #define BE_GFX_VERSION_MINOR 1
-#define BE_GFX_VERSION_REV 41
+#define BE_GFX_VERSION_REV 42
 
 /*!! include('common/version', 'be::gfx') !! 6 */
 /* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
